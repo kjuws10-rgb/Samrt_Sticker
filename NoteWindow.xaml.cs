b997039907b4b1ac555ff4bab13cf4977226b981
kjuws10-rgb <kjuws10-rgb@users.noreply.github.com>
2026-07-26 12:25:48 +0900

    fix: use native window resizing for notes

--- NoteWindow.xaml.cs
+++ NoteWindow.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.IO;
 using System.Windows.Input;
-using System.Windows.Controls.Primitives;
 
 namespace SmartSticker;
 
@@ -70,7 +69,6 @@ public partial class NoteWindow : Window
     private void Strike_Click(object sender, RoutedEventArgs e) => Editor.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, Editor.Selection.GetPropertyValue(Inline.TextDecorationsProperty) == TextDecorations.Strikethrough ? null : TextDecorations.Strikethrough);
     private void Bullets_Click(object sender, RoutedEventArgs e) => EditingCommands.ToggleBullets.Execute(null, Editor);
     private void Editor_TextChanged(object sender, TextChangedEventArgs e) => Save();
-    private void ResizeGrip_DragDelta(object sender, DragDeltaEventArgs e) { Width = Math.Max(MinWidth, Width + e.HorizontalChange); Height = Math.Max(MinHeight, Height + e.VerticalChange); }
     private void SetColor(string color) { _note.Color = color; Root.Background = (System.Windows.Media.Brush)new BrushConverter().ConvertFromString(color)!; }
     private void Save()
     {
